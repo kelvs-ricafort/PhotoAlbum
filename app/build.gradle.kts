e95d55d52469a0a8f81968a198b4a7b42db8ea32
Kelvin Ricafort <kelvs.r@outlook.com>
2023-11-04 14:20:48 +0800

    Add Necessary Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Room Def
+    implementation("androidx.room:room-runtime:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
+    // Live Data
+    implementation("androidx.lifecycle:lifecycle-livedata:2.6.2")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
